f0907f3304df9f495d61f8e763b7d02c90a2e3b5
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-01-10 11:11:11 -0800

    Pair programming. Complete callbackReview

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,27 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  // read file
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      var lines = data.split(/\r?\n/);
+      callback(null, lines[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request.get(url, (err, res, body) => {
+    if (err) {
+      callback (err, null);
+    } else {
+      callback (null, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
