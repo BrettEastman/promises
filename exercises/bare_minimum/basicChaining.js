fff8437fa9c6c012b2d54c24ed38265466f414ad
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-01-10 16:35:22 -0800

    Pair programming. Complete bare minimums for Promises

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,33 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var promiseConstructor = require('./promiseConstructor.js');
+var promisification = require('./promisification.js');
 
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  // reads a GitHub username from a `readFilePath` - on first line of the file
+  return promiseConstructor.pluckFirstLineFromFileAsync(readFilePath)
+    .then(function(username) {
+      if (!username) {
+        throw new Error('no username');
+      } else {
+        return username;
+      }
+    })
+    .then(function(currentUser) {
+      // sends a request to the GitHub API for the user's profile
+
+      return promisification.getGitHubProfileAsync(currentUser);
+    })
+    .then(function(profile) {
+      // then, writes the JSON response of the API to `writeFilePath`
+      return fs.writeFileSync(writeFilePath, JSON.stringify(profile));
+    })
+    .catch(function(err) {
+      console.log('Oops, caught an error: ', err.message);
+    });
 };
 
 // Export these functions so we can test them
